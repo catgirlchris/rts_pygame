236d7678ac2e81544841a92891d469ac761e0b1f
Christianfg99 <47301396+Kyokofg99@users.noreply.github.com>
2022-07-18 20:11:52 +0200

    game window initial position and laptop size

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame as pg
+import os
 from game.game import Game
 
 def main():
     running = True
     playing = True
+    x = 50
+    y = 25
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
     pg.init()
     pg.mixer.init()
-    screen = pg.display.set_mode((1800, 980), flags=pg.RESIZABLE)
+    screen = pg.display.set_mode((1200, 680), flags=pg.RESIZABLE)
     clock = pg.time.Clock()
 
     # implement menus
